15bdecfa1a61d0fa16e2b60c10b1609e4959b992
tristangaebler <tgae6529@740s-pl-115.local>
2016-01-04 09:50:13 -0700

    Added a color changing method

--- SillyApp/ViewController.swift
+++ SillyApp/ViewController.swift
@@ -27,6 +27,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    @IBAction func RandomColor(sender: AnyObject) {
+        let newRed = CGFloat(Double(arc4random_uniform(256)) / 255.000)
+        let newGreen  = CGFloat(Double(arc4random_uniform(256)) / 255.000)
+        let newBlue = CGFloat(Double(arc4random_uniform(256)) / 255.000)
+        
+        view.backgroundColor = UIColor(red: newRed, green: newGreen, blue: newBlue, alpha: 1.0)
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
